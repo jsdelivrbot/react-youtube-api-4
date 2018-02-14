13b972fba8e21a72e8d00a5317269c61288c137f
Michal Przybylski <michalprzybylski3@gmail.com>
2018-02-14 20:58:35 +0100

    first content on screen

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.querySelector('.container');
+
+const App = () => {
+  return <div>Hi!</div>;
+}
+
+
+
+ReactDOM.render(<App />, container);
