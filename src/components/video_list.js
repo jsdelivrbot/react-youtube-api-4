de3f05a00b0eefb44a55e0bb3bae5f4b9e1f2717
Michal-Przybylski <michalprzybylski3@gmail.com>
2018-02-16 16:33:00 +0100

    add comments of how-to react works here

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import VideoListItem from './video_list_item';
 
+// created functional based component to create a life of video
+// videoItems is maping through props.videos which is a search result of a YTsearch function in main app component and its creating new instance od the VideoListItem component with key and a video element
+// this component is returning a unordered list with videoItems array 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
     return <VideoListItem key={video.etag} video={video} />;
