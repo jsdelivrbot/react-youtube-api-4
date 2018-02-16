b22290d2a136a439dc0de085aec237e5a2c61f88
Michal-Przybylski <michalprzybylski@gmail.com>
2018-02-16 16:05:06 +0100

    added some comments of how-to react work also changed SearchBar from functonal component to class based component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';                      // importing React (saving it as a 'React' variable from 'react' (inside node_modules))
+import ReactDOM from 'react-dom';    // importing ReactDOM (saving it as a 'ReactDOM' variable from 'react-dom' (inside node_modules))
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
+import SearchBar from './components/search_bar';     // importing SearchBar component that I made
+import VideoList from './components/video_list';       // importing VideoList component that I made
 
 
-const container = document.querySelector('.container');
-const API_KEY = 'AIzaSyDQKUj2RRG77rzW7N1pzWvkdncz2WNTvos';
+const container = document.querySelector('.container');   
+const API_KEY = 'AIzaSyDQKUj2RRG77rzW7N1pzWvkdncz2WNTvos';    // API KEY that I generated from https://console.developers.google.com/apis/dashboard
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+// main App function that returns a JSX with wrapper div and inside it generates a component Searchbar
+// class based component which uses YTSearch method to fetch data, and then this data is store in this.state.videos
+class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = { videos: [] };
+
+    // only the most parent component should be responsible for fetching data
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList  videos={this.state.videos} />
+      </div >
+    );
+  };
 }
 
-
+// ReactDom render method that renders to the front component App in location which is in a container variable
 ReactDOM.render(<App />, container);
