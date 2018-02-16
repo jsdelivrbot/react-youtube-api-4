8ef751a8ac40254bfb57aac0c5ce2c3fc0a0cc07
Michal-Przybylski <michalprzybylski3@gmail.com>
2018-02-16 17:14:01 +0100

    modified video list item to show content onscreen

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>;
+// ({video}) works like this: the argument is props, then it destructure this props object and takes out a video property and saves it as a variable
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img src={imageUrl} alt="" className="media-object" />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">
+            {video.snippet.title}
+          </div>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default VideoListItem;
